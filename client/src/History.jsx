f70839ff885bbfd380ab7d6bf345d44aa4e41bee
Gabe Decker <42gdeckers@gmail.com>
2024-11-21 21:42:07 -0700

    Copied code from Home into History since there's pretty much the same functionality upon loading in. Need to change it and make a new component for filled antiderivatives

--- client/src/History.jsx
+++ client/src/History.jsx
@@ -1,18 +1,34 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import Antiderivative from './Antiderivative.jsx'
 import './style.css'
 
 function History(props) {
-  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([]);
 
   const user = props.user;
 
+  // TODO: Make FilledAntiderivative component and change this to reflect that
   async function getHistory() {
     const res = await fetch("/history/", {
       credentials: "same-origin"
     });
     const serverHistory = await res.json();
     console.log(serverHistory);
+    let newHistory = [];
+    const keys = Object.keys(serverHistory);
+
+    for (const key of keys) {
+      const data = serverHistory[key];
+      newHistory.push(<div key={data["id"]} className="antiderivative">
+        <Antiderivative
+          data={data}
+          user={user}
+        />
+      </div>);
+    }
+    setHistory(newHistory);
+
   }
 
   useEffect(() => {
@@ -22,6 +38,7 @@ function History(props) {
   return (
     <div id="history-container">
       <h1>Temp History Page</h1>
+      {history}
     </div>
   )
 }
