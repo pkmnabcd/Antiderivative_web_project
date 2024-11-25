39e2a21c099211837c2b03a1855135365f773fdb
Gabe Decker <42gdeckers@gmail.com>
2024-11-24 21:14:19 -0700

    Fixed some stuff in Antiderivative

--- client/src/Antiderivative.jsx
+++ client/src/Antiderivative.jsx
@@ -7,13 +7,14 @@ const constKeys = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7};
 
 function Antiderivative(props) {
   const [constInputs, setConstInputs] = useState({});
+  const [constComponents, setConstComponents] = useState(null);
   const user = props.user;
   const data = props.data;
 
   function setConstInput(key, inputText) {
     let newConstState = constInputs;
     if (inputText === "" || isNaN(parseFloat(inputText))) {
-      newConstState[key] = null;
+      newConstState[key] = undefined;
     } else {
       newConstState[key] = parseFloat(inputText);
     }
@@ -24,28 +25,34 @@ function Antiderivative(props) {
   function handleSubmit() {
   }
 
-  let constantComponents = [];
-  let newConstState = {};
-  const dataKeys = Object.keys(data);
+  useEffect(() => {
+    let newConstantComponents = [];
+    let newConstState = {};
+    const dataKeys = Object.keys(data);
 
-  for (const key of dataKeys) {
-    if (key.length === 1) {
-      constantComponents.push(<div key={constKeys[key]} className="const">
-        <span>Set {key}: </span>
-        <input type="text"
-          value={newConstState[key]}
-          onChange={(e) => setConstInput(key, e.target.value)}
-        />
-      </div>);
+    for (const key of dataKeys) {
+      if (key.length === 1) {
+        newConstState[key] = setConstInput(key, "");
+        newConstantComponents.push(<div key={constKeys[key]} className="const">
+          <span>Set {key}: </span>
+          <input type="text"
+            value={newConstState[key]}
+            onChange={(e) => setConstInput(key, e.target.value)}
+          />
+        </div>);
+      }
     }
-  }
+    setConstInputs(newConstState);
+    setConstComponents(newConstantComponents);
+  }, []);
+
 
   return (
     <>
       <div>{data["latexText"]}</div>
       <MathComponent tex={data["latexText"]} />
       <form className="const-container" onSubmit={handleSubmit}>
-        {constantComponents}
+        {constComponents}
         <button>Get Solution</button>
       </form>
     </>
