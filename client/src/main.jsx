de4f8986d8cea41b1d5fa3205e37d96eae8ffcdc
Gabe Decker <42gdeckers@gmail.com>
2024-12-01 17:26:56 -0600

    Got rid of index.css and moved to style.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './style.css'
 import 'vite/modulepreload-polyfill'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
